1097fd6f8d75b819823c6690ab5fd5d2d04095e6
MrFlowProduction <florian@mrflow.hu>
2025-03-18 09:21:58 +0100

    First commit

---
+++ portainer/install.sh
@@ -0,0 +1,2 @@
+sudo docker volume create portainer_data
+sudo docker compose up -d
---
+++ raspi_install.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade -y
+curl -sSL https://get.docker.com | sh
+
+sudo apt install mc -y
+mkdir portainer
+
